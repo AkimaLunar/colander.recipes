62838cd872c5da8a420e092c7fa696c1b2c6babb
Ria Nicole Carmin <akimalunar@gmail.com>
2017-05-04 17:27:14 -0700

    0.1.0. Code cleanup

--- app/search.js
+++ app/search.js
@@ -2,6 +2,12 @@ var client = algoliasearch('P6BZRHYIM7', 'f1552f8453233d02a898154724697917');
 var recipeCardTemplate = require("../templates/card.hbs");
 var errorTemplate = require("../templates/error.hbs");
 
+// Elements
+var container = 'results';
+var searchForm = '#algoliaSearchForm';
+var searchInput = '#algoliaSearchInput';
+var searchSubmit = '#algoliaSearchSubmit';
+
 // Rendering Templates
 function recipesHTML(recepies) {
   var resultsContainer = document.getElementById("results");
@@ -10,18 +16,30 @@ function recipesHTML(recepies) {
 function errorHTML() {
   var resultsContainer = document.getElementById("results");
   resultsContainer.innerHTML = errorTemplate();
+  console.log('nothing found');
+
 }
 
 // Algolia Search
 var index = client.initIndex('recipes');
 
+function searchIndex(input, resultHtml, errorHtml) {
+  var query = $(input).val();
+  index.search(query, function(err, content) {
+    if (content.hits.length < 1) {
+      errorHtml();
+    }
+    resultHtml(content);
+  });
+}
+
+// Event Listeners
 $('#algoliaSearchForm').submit(function(e){
   e.preventDefault();
-  var query = ($('#algoliaSearchInput').val());
-  index.search(query, function(err, content) {
-    if (content.hits.lenght < 1) {
-      errorHTML();
-    }
-    recipesHTML(content);
-  });
+  searchIndex(searchInput, recipesHTML, errorHTML);
 })
+
+$('#algoliaSearchInput').keyup( function() {
+  if ( this.value.length < 4 ) return;
+  searchIndex(searchInput, recipesHTML, errorHTML);
+});
